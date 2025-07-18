ac30c7ccf6b79bd09b4d04c95ca553f2b6c488d3
Samuel Sheehy <samuelsheehy95@gmail.com>
2025-07-18 08:21:10 +0100

    Add fetch

---
+++ fetch.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+import datetime
+from decimal import Decimal
+
+
+from parse import parse_lottery_html
+
+
+class AbstractSource(ABC):
+
+    @abstractmethod
+    def get(self) -> str: ...
+
+
+@dataclass
+class Game:
+    next_draw_date: datetime.date
+    jackpot: int
+
+
+@dataclass
+class Fetcher:
+    source: AbstractSource
+
+    def fetch(self) -> dict[str, Game]:
+        # Simulated fetch logic
+        html_content = self.source.get()
+        games_data = parse_lottery_html(html_content)
+        result = {}
+        for name, data in games_data.items():
+            data['next_draw_date'] = datetime.datetime.strptime(data['next_draw_date'], '%d-%m-%Y').date()
+            data['jackpot'] = Decimal(data['jackpot'].replace('£', '').replace(',', '').replace('M', 'E6'))
+            result[name] = Game(data['next_draw_date'], int(data['jackpot']))
+        return result
---
+++ tests/test_fetch.py
@@ -0,0 +1,29 @@
+import datetime
+
+from fetch import AbstractSource, Fetcher
+
+
+class FakeSource(AbstractSource):
+
+    def __init__(self):
+        with open("tests/national_lottery_games.html", "r") as file:
+            self.content = file.read()
+
+    def get(self):
+        return self.content
+
+
+def test_fetch():
+    source = FakeSource()
+    fetcher = Fetcher(source=source)
+
+    result = fetcher.fetch()
+
+    game = result.get('euromillions')
+    assert game is not None
+    assert game.next_draw_date == datetime.date(2023, 10, 27)
+    assert game.jackpot == 14_000_000
+
+
+
+
