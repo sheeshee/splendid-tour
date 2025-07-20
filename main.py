b381f458fe6698a68a2c1f182290604bf996b104
Samuel Sheehy <samuelsheehy95@gmail.com>
2025-07-20 15:17:51 +0100

    Add script to call webhook when conditions met

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+
+load_dotenv()
+
+
+if __name__ == "__main__":
+    from fetch import NationalLotterySource, Fetcher
+
+    source = NationalLotterySource()
+    fetcher = Fetcher(source=source)
+    games = fetcher.fetch()
+    message = ""
+
+    # National Lottery
+    if games["lotto"].roll_count > 5:
+        message += f"Lotto must be won! Next draw: {games['lotto'].next_draw_date}, Jackpot: {games['lotto'].jackpot}"
+
+    if games["euromillions"].jackpot >= 100_000_000:
+        message += f"Euromillions jackpot is over £100 million! Next draw: {games['euromillions'].next_draw_date}, Jackpot: {games['euromillions'].jackpot}"
+
+    requests.post(os.environ["WEBHOOK"], data=message.encode(encoding="utf-8"))
